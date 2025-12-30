595d07862e9d1fea95ae2578bcf53d9630d1c611
silvijusm <silvijusm@gmail.com>
2025-12-30 17:47:42 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+﻿console.log("Pickleball PWA veikia");
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("sw.js")
+      .then(() => console.log("Service Worker užregistruotas"))
+      .catch(err => console.error("SW klaida", err));
+  });
+}
